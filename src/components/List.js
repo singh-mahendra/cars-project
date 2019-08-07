07e30180f4d0ce2397598526b1cebbe45c54c8bf
Mahendra Singh <mahendra3006@outlook.com>
2019-08-07 10:05:35 +0530

    fixed css issues

--- src/components/List.js
+++ src/components/List.js
@@ -4,9 +4,13 @@ import PropTypes from 'prop-types';
 const List = (props) =>{
     return (
         <ul>
-            {props.listItems.map((listItem) => {
-                return (<li key={listItem[props.itemKeyProperty]}>{props.itemComponent(listItem)}</li>);
-            })}
+            {
+                props.listItems && props.listItems.length ?
+                    props.listItems.map((listItem) => {
+                    return (<li key={listItem[props.itemKeyProperty]}>{props.itemComponent(listItem)}</li>);
+                    })
+                    : "No results match your criteria."
+        }
         </ul>
     )
 }
